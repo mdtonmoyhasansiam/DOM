3bb8a9b00839483bd3da95d9ad44ef7b9465a94f
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-21 14:25:00 +0600

    Practice DOM Manipulate

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello JavaScript");
+const headerText = document.getElementById('header');
+// console.log(headerText);
+headerText.innerText = 'I changed it';
+
+// const textClass = document.getElementsByClassName('text');
+// // console.log(textClass);
+// for(let text of textClass){
+//     text.innerText = 'I changed it';
+// }
+
+const textClass = document.querySelectorAll('.text');
+for(let text of textClass){
+    text.innerText = 'This is P tag'
+}
+
+const addedTag = document.getElementById('add-p-tag');
+addedTag.innerHTML = '<p>Finally Added</p>'
