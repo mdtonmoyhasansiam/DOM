ef2693c7a4161483b51eab4b03e668ceb9329f87
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-21 14:39:12 +0600

    Explore Both and Single class will change

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const headerText = document.getElementById('header');
 // console.log(headerText);
 headerText.innerText = 'I changed it';
 
+
 // const textClass = document.getElementsByClassName('text');
 // // console.log(textClass);
 // for(let text of textClass){
 //     text.innerText = 'I changed it';
 // }
 
+
 const textClass = document.querySelectorAll('.text');
+// Both class will change use 'for of loop'
 for(let text of textClass){
     text.innerText = 'This is P tag'
 }
+// Single class will change
+textClass[0].innerText = '1st Class'
+textClass[1].innerText = '2nd Class'
+textClass[2].innerText = '3rd Class'
+
 
 const addedTag = document.getElementById('add-p-tag');
 addedTag.innerHTML = '<p>Finally Added</p>'
